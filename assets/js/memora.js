0716c0e76c1fe730b40f2546594b22bd00bcbe0d
Jonathan Palardy <jonathan.palardy@gmail.com>
2019-12-06 21:48:05 -0800

    Update generated memora.js

--- assets/js/memora.js
+++ assets/js/memora.js
@@ -213,12 +213,12 @@ exports.save = function (decks) {
     body: JSON.stringify(updatedDecks)
   });
 };
-},{"./scheduler":"TuEg"}],"FO+Z":[function(require,module,exports) {
+},{"./scheduler":"TuEg"}],"FOZT":[function(require,module,exports) {
 function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
 
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
 
-function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
@@ -391,7 +391,7 @@ exports.handle = function (app, backend) {
     }
   }, false);
 };
-},{"./utils":"FO+Z"}],"3hpa":[function(require,module,exports) {
+},{"./utils":"FOZT"}],"hpaf":[function(require,module,exports) {
 /* global window, document */
 function debounce(f, wait) {
   var timeoutId = null;
@@ -436,7 +436,7 @@ function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArra
 
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
 
-function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
@@ -603,4 +603,4 @@ Vue.filter("pluralize", function (count, singular) {
 
 keyboard.handle(app, backend);
 window.app = app;
-},{"./scheduler":"TuEg","./backend":"bRH5","./keyboard":"VgNd","./browser":"3hpa"}]},{},["A2T1"], null)
+},{"./scheduler":"TuEg","./backend":"bRH5","./keyboard":"VgNd","./browser":"hpaf"}]},{},["A2T1"], null)
